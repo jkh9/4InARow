9df1daba651988fcf1b32141e45c25618f450790
MOISEX-A-PINXO\jkh9L <jkh91234@gmail.com>
2018-05-09 18:05:26 +0200

    Completed 4 in a row

---
+++ 4inARow/FourInARow.cs
@@ -0,0 +1,167 @@
+﻿//Moisés Encinas, Marcos Cervantes, Jose Vilaplana
+
+using System;
+using System.Text;
+
+class FourInARow
+{
+    static void Main(string[] args)
+    {
+        string[] map =
+        {
+            "| | | | | | | |",
+            "---------------",
+            "| | | | | | | |",
+            "---------------",
+            "| | | | | | | |",
+            "---------------",
+            "| | | | | | | |",
+            "---------------",
+            "| | | | | | | |",
+            "---------------",
+            "| | | | | | | |",
+            "---------------",
+            "| | | | | | | |",
+            "---------------",
+        };
+
+        bool turn = true;
+        byte position = 0;
+
+        WriteMap(map);
+
+        do
+        {
+            //1.-Check user input
+            position = GetNumber();
+
+            //2.-Check collisions
+            Position(position, ref map, turn);
+
+            //3.-Write map
+            WriteMap(map);
+
+            //4.-Cambio de turno
+            turn = !turn;
+
+        } while (!WinnedX(map) && !WinnedY(map));
+
+        if (WinnedX(map))
+        {
+            Console.WriteLine("Winned the X");
+        }
+        else
+        {
+            Console.WriteLine("Winned the O");
+        }
+    }
+
+    public static void WriteMap(string[] map)
+    {
+        Console.Clear();
+        for (int i = 0; i < map.Length; i++)
+        {
+            Console.WriteLine(map[i]);
+        }
+    }
+
+    public static byte GetNumber()
+    {
+        ConsoleKeyInfo number;
+        number = Console.ReadKey(false);
+        switch (number.Key)
+        {
+            case ConsoleKey.D1: return 1;
+            case ConsoleKey.D2: return 3;
+            case ConsoleKey.D3: return 5;
+            case ConsoleKey.D4: return 7;
+            case ConsoleKey.D5: return 9;
+            case ConsoleKey.D6: return 11;
+            case ConsoleKey.D7: return 13;
+            default: return 0; //Incase the input its not correct, do nothing.
+        }
+    }
+
+    public static bool WinnedX(string[] map)
+    {
+        for (int i = 0; i < map.Length; i++)
+        {
+            if (map[i].Contains("x|x|x|x"))
+            {
+                return true;
+            }
+        }
+
+        map = Reverse(map);
+
+        for (int i = 0; i < map.Length; i++)
+        {
+            if (map[i].Contains("x-x-x-x"))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static bool WinnedY(string[] map)
+    {
+        for (int i = 0; i < map.Length; i++)
+        {
+            if (map[i].Contains("o|o|o|o"))
+            {
+                return true;
+            }
+        }
+
+        map = Reverse(map);
+
+        for (int i = 0; i < map.Length; i++)
+        {
+            if (map[i].Contains("o-o-o-o"))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static string[] Reverse(string[] map)
+    {
+        string[] newMap = new string[map[0].Length];
+
+        for (int i = 0; i < map[0].Length; i++)
+        {
+            string line = "";
+            for (int a = 0; a < map.Length; a++)
+            {
+                line += map[a][i];
+            }
+            newMap[i] = line;
+        }
+
+        return newMap;
+    }
+
+    public static void Position(byte position, ref string[] map, bool turn)
+    {
+        for (int i = 12; i >= 0; i -= 2)
+        {
+            if (map[i][position] == ' ')
+            //Position x2 cause the ' | ' are in the middle
+            {
+                string aux = map[i];
+                StringBuilder modificableString = new StringBuilder(aux);
+                if (turn == true)
+                    modificableString[position] = 'o';
+                else
+                    modificableString[position] = 'x';
+                aux = modificableString.ToString();
+                map[i] = aux;
+                break;
+            }
+        }
+    }
+}
